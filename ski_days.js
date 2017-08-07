b54a43f2b1e3021f67d153ad062d219691a24045
Fabien Pannatier <hi@fabienpannatier.ch>
2017-08-07 16:41:43 +0200

    replcae createP

--- ski_days.js
+++ ski_days.js
@@ -3,6 +3,16 @@ var back;
 var suntan;
 var tanning = 0;
 
+//snow variables
+var quantity = 100;
+var xPosition = [];
+var yPosition = [];
+var flakeSize = [];
+var direction = [];
+var minFlakeSize = 1;
+var maxFlakeSize = 10;
+var snowColor = 255;
+
 function preload() {
   phone = loadImage('assets/phone.png');
   back = loadImage('assets/back.jpg');
@@ -10,15 +20,24 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(750,1334);
+  createCanvas(750,1380);
   background(0);
-  createP('Skitage').position(370,1310);
+  //createP('Skitage').position(370,1310);
   sliderTan = createSlider(0,255,0);
-  sliderTan.position(320,1300);
+  sliderTan.position(310,1350);
   sliderTan.input(setTanning);
 
-  //background(0);
-  noLoop();
+
+
+  //snow setup
+  frameRate(25);
+  for(var i = 0; i < quantity; i++) {
+    flakeSize[i] = round(random(minFlakeSize, maxFlakeSize));
+    xPosition[i] = random(0, width);
+    yPosition[i] = random(0, height);
+    direction[i] = round(random(0, 1));
+  }
+
 
 }
 
@@ -27,27 +46,54 @@ function draw() {
 
   //var tanning = sliderTan.value();
   image(back,103,181,544,967);
-  image(phone,0,0,width,height);
+  fill(255);
+  drawSnow();
+  image(phone,0,0,width,1334);
 
   textSize(25);
+  fill(255);
   text('Skitage: '+sliderTan.value(),320,300);
 
 
   fill(255);
   noStroke();
   rect(0,0,width,10);
-  rect(0,height-10,width,10);
+  rect(0,height-50,width,60);
   rect(width-10,0,10,height);
 
   push();
   tint(255,tanning);
   image(suntan,280.5,635,256/1.37,235/1.37);
   pop();
-  
+  fill(0);
+  textSize(35);
+  text('Skitage',320,1340);
+
 }
 
 function setTanning(){
   tanning = sliderTan.value();
   console.log('tanning',tanning);
   redraw();
+
+}
+
+function drawSnow() {
+	for(var i = 0; i < xPosition.length; i++) {
+    noStroke();
+    ellipse(xPosition[i], yPosition[i], flakeSize[i], flakeSize[i]);
+
+    if(direction[i] == 0) {
+      xPosition[i] += map(flakeSize[i], minFlakeSize, maxFlakeSize, .1, .5);
+    } else {
+      xPosition[i] -= map(flakeSize[i], minFlakeSize, maxFlakeSize, .1, .5);
+    }
+
+    yPosition[i] += flakeSize[i] + direction[i];
+
+    if(xPosition[i] > width + flakeSize[i] || xPosition[i] < -flakeSize[i] || yPosition[i] > height + flakeSize[i]) {
+      xPosition[i] = random(0, width);
+      yPosition[i] = -flakeSize[i];
+    }
+  }
 }
