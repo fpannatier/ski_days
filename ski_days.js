422b4892e7b66bda79b8f0dc86270ee484362e55
chrischne <chrischne@gmail.com>
2017-08-07 15:32:35 +0200

    push pop around tint

--- ski_days.js
+++ ski_days.js
@@ -15,7 +15,7 @@ function setup() {
   createP('Skitage').position(370,1310);
   sliderTan = createSlider(0,255,0);
   sliderTan.position(320,1300);
-  sliderTan.input(setTanning)
+  sliderTan.input(setTanning);
 
   //background(0);
   noLoop();
@@ -24,22 +24,30 @@ function setup() {
 
 function draw() {
 
+
   //var tanning = sliderTan.value();
   image(back,103,181,544,967);
   image(phone,0,0,width,height);
+
+  textSize(25);
+  text('Skitage: '+sliderTan.value(),320,300);
+
+
   fill(255);
   noStroke();
   rect(0,0,width,10);
   rect(0,height-10,width,10);
   rect(width-10,0,10,height);
+
+  push();
   tint(255,tanning);
   image(suntan,280.5,635,256/1.37,235/1.37);
-  textSize(25);
-  text('Skitage: '+sliderTan.value(),320,300);
-
+  pop();
+  
 }
 
 function setTanning(){
   tanning = sliderTan.value();
+  console.log('tanning',tanning);
   redraw();
 }
